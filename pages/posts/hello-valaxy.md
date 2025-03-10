086cb8bd9f1c10c83328a03d3d95fd2d06a464d0
朱英豪 <17673298578@163.com>
2025-03-10 19:36:42 +0800

    first commit

---
+++ pages/posts/hello-valaxy.md
@@ -0,0 +1,17 @@
+---
+title: 面试题
+date: 2025-03-10
+updated: 2025-03-10
+categories: 面试题
+tags:
+  - 面试题
+top: 1
+---
+
+## Valaxy
+
+面试题面试题面试题面试题面试题面试题面试题
+
+## Usage
+
+Modify `valaxy.config.ts` to custom your blog.
---
+++ valaxy.config.ts
@@ -0,0 +1,48 @@
+import type { UserThemeConfig } from 'valaxy-theme-yun'
+import { defineValaxyConfig } from 'valaxy'
+
+// add icons what you will need
+const safelist = [
+  'i-ri-home-line',
+]
+
+/**
+ * User Config
+ */
+export default defineValaxyConfig<UserThemeConfig>({
+  // site config see site.config.ts
+
+  theme: 'yun',
+
+  themeConfig: {
+    banner: {
+      enable: true,
+      title: '我是朱英豪',
+    },
+
+    pages: [
+      {
+        name: '日记',
+        url: '/links/',
+        icon: 'i-ri-book-2-line',
+        color: 'dodgerblue',
+      },
+      // {
+      //   name: '备忘录',
+      //   url: '/girls/',
+      //   icon: 'i-ri-women-line',
+      //   color: 'hotpink',
+      // },
+    ],
+
+    footer: {
+      since: 2016,
+      beian: {
+        enable: true,
+        icp: '苏ICP备17038157号',
+      },
+    },
+  },
+
+  unocss: { safelist },
+})
