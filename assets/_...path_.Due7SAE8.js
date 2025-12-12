84078e8c318df72ef006dfa40fb986b8b740a23a
zyh8798 <zyh8798@users.noreply.github.com>
2025-12-12 10:02:06 +0000

    deploy: 915f23e21d0a07a0cf18d1db0485307e61b5d36d

---
+++ assets/_...path_.Due7SAE8.js
@@ -0,0 +1,1 @@
+import{a,u as s,e as t}from"./chunks/vue-router.BeoKCBYL.js";import{f as n,H as r,P as c,O as p}from"./framework.Dx7sWfGO.js";const u=n({__name:"[...path]",setup(f){const o=s(),e=a();return r(()=>{e.path!=="/"&&e.path.endsWith("/")&&o.replace(e.path.slice(0,-1))}),(i,m)=>(p(),c("div"))}});typeof t=="function"&&t(u);export{u as default};
---
+++ assets/index.BQ8F5uvI.js
@@ -0,0 +1,1 @@
+import{C as s}from"./app.CNWqKRbt.js";import"./chunks/dayjs.DRNXKsfK.js";import"./animation.BZ223lSZ.js";const a="/assets/none.DLM1AMsD.jpg";function c(t,o=a){const r=t.target;r.setAttribute("data-src",r.src),r.src=o}function i(t){return s(t,{template:"YYYY-MM-DD HH:mm:ss"})}export{i as f,c as o};
